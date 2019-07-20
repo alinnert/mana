1fbcbbe3488f62bb301118784e11111efbbf0e3c
Andreas Linnert <andreas@linnertmedia.de>
2019-07-20 23:20:48 +0200

    Updated ESLint config to fully support TypeScript

--- packages/core/src/api/watch-selector.ts
+++ packages/core/src/api/watch-selector.ts
@@ -1,8 +1,9 @@
-import { registerSelector } from "../core/mutation-observer";
+import { registerSelector } from '../core/mutation-observer'
+import { returnValue } from '../utils'
 
-//#region types
-export type WatchSelectorContext = {
-  [key: string]: any
+// #region types
+export interface WatchSelectorContext {
+  [key: string]: unknown;
 }
 
 export type WatchSelectorCallback = (
@@ -10,25 +11,34 @@ export type WatchSelectorCallback = (
   context: WatchSelectorContext
 ) => void
 
-export type WatchSelectorOptions = {
-  selector: string | null
-  context?: HTMLElement
-  onAdded?: WatchSelectorCallback
-  onRemoved?: WatchSelectorCallback
-  onAttributeChanged?: WatchSelectorCallback
+export interface WatchSelectorOptions {
+  selector: string | null;
+  context?: HTMLElement;
+  onAdded?: WatchSelectorCallback;
+  onRemoved?: WatchSelectorCallback;
+  onAttributeChanged?: WatchSelectorCallback;
 }
-//#endregion
+// #endregion
 
 const watchSelectorDefaultOptions: WatchSelectorOptions = {
   selector: null,
   context: document.body,
-  onAdded: () => {},
-  onRemoved: () => {},
-  onAttributeChanged: () => {}
+  onAdded: returnValue(undefined),
+  onRemoved: returnValue(undefined),
+  onAttributeChanged: returnValue(undefined)
 }
 
-export function watchSelector(userOptions: WatchSelectorOptions) {
-  const { selector, ...options } = Object.assign({}, watchSelectorDefaultOptions, userOptions)
+/**
+ * Add a new watcher. Watches for specific elements and executes functions
+ * on addition and removal from DOM, and also on attribute manipulation.
+ * @param userOptions Options to define the watcher.
+ */
+export function watchSelector (userOptions: WatchSelectorOptions): void {
+  const { selector, ...options } = Object.assign(
+    {},
+    watchSelectorDefaultOptions,
+    userOptions
+  )
 
   registerSelector(selector, options)
 }
